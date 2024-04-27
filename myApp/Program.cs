70a6cc9ff1152bcebe28678d079ce01287ddff43
bnelford <nelfordbc@gmail.com>
2024-04-27 15:30:27 -0600

    initial

---
+++ myApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using myLibrary;
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Calculator calc = new Calculator();
+            Calculator calc1 = new Calculator();
+            Calculator calc2 = new Calculator();
+            Calculator calc3 = new Calculator();
+            Calculator calc4 = new Calculator();
+
+            calc.Add(42);
+
+            Console.WriteLine(calc.Sum);
+
+            Console.WriteLine(Calculator.UsageCount);
+        }
+    }
+}
---
+++ myLibrary/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace myLibrary
+{
+    [XmlRoot("calc", Namespace="urn:foo")]
+    public class Calculator
+    {
+        public double Sum { get; set; }
+        public static int UsageCount { get; private set; }
+
+        static Calculator()
+        {
+            Console.WriteLine("Static ctor fired");
+        }
+
+
+        public void Add(double a)
+        {
+            ++UsageCount;
+            this.Sum += a;
+        }
+
+    }
+}
